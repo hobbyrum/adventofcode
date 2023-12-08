e206b220fe38efdd7649bf26a9dd065ce60d074a
hobbyrum <9680372+hobbyrum@users.noreply.github.com>
2023-12-08 14:02:42 +0100

    Create day01.ps1

---
+++ day01.ps1
@@ -0,0 +1,31 @@
+$position = [PSCustomObject]@{
+    Depth = 0
+    HorizontalPosition = 0
+}
+
+function Forward {
+    param($times = 0)
+
+    for ($i = 0; $i -lt $times; $i++) {
+        $position.HorizontalPosition++
+        # Write-Host $position.HorizontalPosition
+    }
+}
+
+function Down {
+    param($times = 0)
+
+    for ($i = 0; $i -lt $times; $i++) {
+        $position.Depth++
+        # Write-Host $position.Depth
+    }
+}
+
+function Up {
+    param($times = 0)
+
+    for ($i = 0; $i -lt $times; $i++) {
+        $position.Depth--
+        # Write-Host $position.Depth
+    }
+}
